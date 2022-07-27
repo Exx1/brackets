95df351c056ae83f1075b0ebff4ba42ab4af1a57
Exx1 <exx-3@yandex.ru>
2022-07-27 11:36:44 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if ((str[i] === '}' & arr[arr.length - 1] === '{') || (str[i] === ']' & arr[arr.length - 1] === '[') || (str[i] === ')' & arr[arr.length - 1] ==='(') || (str[i] === '2' & arr[arr.length - 1] === '1') || (str[i] === '4'& arr[arr.length - 1] === '3') || (str[i] === '6' & arr[arr.length - 1] === '5') || (str[i] === '|' & arr[arr.length - 1] === '|') || (str[i] === '7' & arr[arr.length - 1] === '7') || (str[i] === '8' & arr[arr.length - 1] === '8')) {
+      arr.pop();
+    } else if ((str[i] === '}' & arr[arr.length - 1] !== '{') || (str[i] === ']' & arr[arr.length - 1] !== '[') || (str[i] === ')' & arr[arr.length - 1] !=='(')) {
+      return false;
+    } else {
+      arr.push(str[i]);
+    }
+  }
+
+  if (arr.length !== 0) {
+    return false;
+  }
+  return true;
 }
+
